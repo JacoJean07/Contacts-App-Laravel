eea9b565e046e3fb0eaf1b87b64651cecfafc09b
jacojean01 <jeancarlos10171@gmail.com>
2024-04-12 13:39:44 -0500

    routes and csrf tokens for forms

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Http\Client\Request;
+use Illuminate\Http\Request as HttpRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,4 +23,17 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
+// existen dos metodo para crear funciones cuando es un return sin mucha logica, se puede hacer de la siguiente manera
+Route::get('/contact', fn () => Response::view('contact'))->middleware('auth');
+// o de la siguiente manera
+Route::get('/contact', function () {
+    return Response::view('contact');
+})->middleware('auth');
+
+// en la funcion se tiene que exportar el request que sea httprequest para poder usarlo
+Route::post('/contact', function (HttpRequest $request) {
+    // en php vanila puedes hacer un var_dump($_POST) para ver los datos que se envian o imprimirlos, sin embargo en laravel se puede hacer de la siguiente manera
+    dd($request->get('name'), 200); // tambien puedes definir el codigo de estado de la respuesta como 404, 200, 500, etc.
+    // dd significa dump and die, es decir, imprime y muere, es una funcion de laravel que te permite imprimir y detener la ejecucion del codigo
+})->middleware('auth');
