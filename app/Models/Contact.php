22aa945516502ffb0212154460144f9e4ed1f84b
jacojean01 <jeancarlos10171@gmail.com>
2024-04-12 14:44:02 -0500

    MVC for create and stored contact in a contact table

---
+++ app/Models/Contact.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+    // se tiene que especificar los campos que se pueden llenar para que no se pueda inyectar codigo
+    // esto se hace porque laravel es muy permisivo y si no se especifica se puede inyectar codigo
+    // aparte de eso estamos usando un csrf token por lo que dara un error al no identificar el campo
+    // csrf en la base de datos
+    protected $fillable = [
+        'name',
+        'phone_number'
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Http\Client\Request;
-use Illuminate\Http\Request as HttpRequest;
+use App\Http\Controllers\ContactController;
+use App\Models\Contact;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
@@ -24,16 +25,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
-// existen dos metodo para crear funciones cuando es un return sin mucha logica, se puede hacer de la siguiente manera
-Route::get('/contact', fn () => Response::view('contact'))->middleware('auth');
-// o de la siguiente manera
-Route::get('/contact', function () {
-    return Response::view('contact');
-})->middleware('auth');
-
-// en la funcion se tiene que exportar el request que sea httprequest para poder usarlo
-Route::post('/contact', function (HttpRequest $request) {
-    // en php vanila puedes hacer un var_dump($_POST) para ver los datos que se envian o imprimirlos, sin embargo en laravel se puede hacer de la siguiente manera
-    dd($request->get('name'), 200); // tambien puedes definir el codigo de estado de la respuesta como 404, 200, 500, etc.
-    // dd significa dump and die, es decir, imprime y muere, es una funcion de laravel que te permite imprimir y detener la ejecucion del codigo
-})->middleware('auth');
+// se esta creando una ruta para el metodo create del controlador ContactController
+Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+// se esta creando una ruta para el metodo store del controlador ContactController
+Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
